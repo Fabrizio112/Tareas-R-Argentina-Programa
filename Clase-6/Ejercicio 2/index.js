f9c8c308818dcc4e44cfda22743a05a096b2313c
Fabrizio112 <fabri.avila3@gmail.com>
2023-02-23 08:41:48 -0300

    Termina ejercicio 2 de la Clase 6
    Se finalizo el ejercicio 2 de la clase 6 el cual constaba de:
    Realizar una interfaz que permita agregar o quitar inputs y permita ingresar el salario anual de un grupo familiar y reflejar los resultados en pantalla mostrando el menor,mayor,Promedio anual y mensual tambien
    Se complico un poco al momento de pensar la solucion para no tomar en cuenta los inputs vacios pero creo que se llego al resultado esperado de una muy buena forma;todavia falta mucho pero muy emocionado por el proceso de aprendizaje y de ejercitacion

--- Clase-6/Ejercicio 2/index.js
+++ Clase-6/Ejercicio 2/index.js
@@ -6,3 +6,109 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor sala
 
 Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
 */
+let $botonAgregar = document.querySelector("#agregar");
+let $botonQuitar = document.querySelector("#quitar");
+let $botonCalcular = document.querySelector("#calcular");
+let iteracionesConLosInputs = 0;
+$botonAgregar.onclick = function () {
+    agregarInputsParaIngresarSalario(iteracionesConLosInputs);
+    iteracionesConLosInputs++;
+    return false;
+}
+
+$botonQuitar.onclick = function () {
+    eliminarInputsParaIngresarSalario(iteracionesConLosInputs);
+    iteracionesConLosInputs--;
+    return false;
+}
+
+function agregarInputsParaIngresarSalario(i) {
+    let nuevoDiv = document.createElement("div");
+    nuevoDiv.className = `minibox`;
+    let nuevoLabel = document.createElement("label");
+    let nuevoLabelTexto = document.createTextNode(`Ingrese su salario Anual Persona ${i + 1}`);
+    nuevoLabel.appendChild(nuevoLabelTexto);
+    let nuevoInput = document.createElement("input");
+    nuevoInput.type = "number";
+    nuevoInput.className = `input`;
+    nuevoDiv.appendChild(nuevoLabel);
+    nuevoDiv.appendChild(nuevoInput);
+    document.querySelector(".container-inputs").appendChild(nuevoDiv);
+
+}
+function eliminarInputsParaIngresarSalario(i) {
+    let arrayCotenedores = document.querySelectorAll(".minibox");
+    arrayCotenedores[i - 1].remove();
+}
+
+
+$botonCalcular.onclick = function () {
+    let $filtroInputs = document.querySelectorAll(".input");
+    for (let x = 0; x < $filtroInputs.length; x++) {
+        if (Number($filtroInputs[x].value) == 0) {
+            $filtroInputs[x].className = "inputNoTomadoEnCuenta";
+        }
+    }
+    let $valorInputs = document.querySelectorAll(".input");
+    let valorDeLosInputs = [];
+    for (let i = 0; i < $valorInputs.length; i++) {
+        valorDeLosInputs[i] = Number($valorInputs[i].value);
+    }
+    document.querySelector("#mayor-anual").textContent = `El mayor salario anual es : ${mayorSalarioAnual(valorDeLosInputs)}`;
+    document.querySelector("#menor-anual").textContent = `El menor salario anual es : ${menorSalarioAnual(valorDeLosInputs)}`;
+    document.querySelector("#anual-promedio").textContent = `El salario anual promedio es : ${salarioAnualPromedio(valorDeLosInputs)}`;
+    document.querySelector("#mensual-promedio").textContent = `El salario mensual promedio es : ${salarioMensualPromedio(valorDeLosInputs)}`;
+    document.querySelector(".resultado").style.display = "block";
+    return false
+}
+
+let mayor = 0;
+let menor;
+let promedioAnual;
+let promedioMensual;
+
+function mayorSalarioAnual(a) {
+    for (let i = 0; i < a.length; i++) {
+        if (mayor < a[i]) {
+            mayor = a[i];
+        } else {
+        }
+    }
+    return mayor;
+}
+function menorSalarioAnual(a) {
+    let aux = 1;
+    for (let i = 0; i < a.length; i++) {
+        if (aux === 1) {
+            menor = a[i];
+            aux = 0;
+        } else if (menor > a[i]) {
+            menor = a[i];
+        }
+    }
+    return menor;
+}
+function salarioAnualPromedio(a) {
+    let contadorAnual = 0
+    for (let x = 0; x < a.length; x++) {
+        contadorAnual += a[x];
+    }
+    promedioAnual = contadorAnual / a.length;
+    return promedioAnual;
+}
+function salarioMensualPromedio(a) {
+    let contadorMensual = 0
+    for (let x = 0; x < a.length; x++) {
+        contadorMensual += (a[x] / 12);
+    }
+    promedioMensual = contadorMensual / a.length;
+    return promedioMensual;
+}
+function noTenerEnCuentaInputsVacios(iteracionesConLosInputs, $valorInputs) {
+    /* for (let i = iteracionesConLosInputs; i > 0; i--) {
+        if (Number($valorInputs[i].value) == 0) {
+            $valorInputs[i].className = "inputNoTomadoEnCuenta";
+        }
+    } */
+    console.log($valorInputs)
+}
