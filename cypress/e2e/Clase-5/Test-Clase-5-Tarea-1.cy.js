09367785a8ab051ae4177f38ac944d199ff905e9
Fabrizio112 <fabri.avila3@gmail.com>
2023-03-16 15:55:22 -0300

    Agrega Test de Errroes
    Se agrega el test que valida los cambios que se producen cuando hay un error en los ingresos de los inputs

--- cypress/e2e/Clase-5/Test-Clase-5-Tarea-1.cy.js
+++ cypress/e2e/Clase-5/Test-Clase-5-Tarea-1.cy.js
@@ -1,19 +1,38 @@
-describe('Test de la Clase 5 de la Tarea 1', () => {
-    beforeEach('Visitar la pagina ', () => {
-        cy.visit('http://192.168.100.28:8080/Clase-5/')
-    })
-    it('Probar si la calculadora funciona de forma correcta', () => {
-        cy.get('#salario-user').type(850000);
-        cy.get('#calcular').click();
-        expect(cy.get('#salario-mensual').should("value", (850000 / 12)));
-    })
-    it('Probar si el boton de resetear funciona de forma correcta', () => {
-        cy.get('#salario-user').type(12000);
-        cy.get('#calcular').click();
-        expect(cy.get('#salario-mensual').should("value", 1000));
-        cy.get('[type="reset"]').click()
-        expect(cy.get('#salario-user').should("value", ""))
-        expect(cy.get('#salario-mensual').should("value", ""))
-    })
+describe('Class 5 Homework 1 Tests', () => {
 
+    describe('Working basic test', () => {
+        beforeEach('Go to the URL ', () => {
+            cy.visit('http://192.168.100.28:8080/Clase-5/')
+        })
+        it('Test if the calculator is working good', () => {
+            cy.get('#salario-user').type(850000);
+            cy.get('#calcular').click();
+            expect(cy.get('#salario-mensual').should("value", (850000 / 12)));
+        })
+        it('Test if the button reset is working good', () => {
+            cy.get('#salario-user').type(12000);
+            cy.get('#calcular').click();
+            expect(cy.get('#salario-mensual').should("value", 1000));
+            cy.get('[type="reset"]').click()
+            expect(cy.get('#salario-user').should("value", ""))
+            expect(cy.get('#salario-mensual').should("value", ""))
+        })
+
+    })
+    describe('A lot of more difficult tests', () => {
+        beforeEach('Go to the URL ', () => {
+            cy.visit('http://192.168.100.28:8080/Clase-5/')
+        })
+        it('Test error calculator', () => {
+            cy.get('#salario-user')
+                .type(",");
+            cy.get('#calcular').click();
+            cy.get('#salario-user')
+                .should("have.class", "border-danger")
+                .and("have.class", "border")
+                .and("have.class", "border-3")
+            cy.get('#contenedor-errores').should("be.visible")
+            cy.get('#contenedor-errores li').should("have.text", 'Este campo solo puede tener numeros enteros');
+        })
+    })
 })
