a44e490528245fae1bb1bac1597520d0b2c77c32
Fabrizio112 <fabri.avila3@gmail.com>
2023-03-16 10:21:21 -0300

    Agrega archivos iniciales a donde se van a escribir las pruebas de los diferentes Ejercicios

---
+++ cypress/e2e/Clase-5/Test-Clase-5-Ejercicio-2.cy.js
@@ -0,0 +1,5 @@
+describe('Test de la Clase 5 del Ejercicio 1', () => {
+    it('Visitar la pagina ', () => {
+        cy.visit('http://192.168.100.28:8080/Clase-5/Ejercicio2/')
+    })
+})
---
+++ cypress/e2e/Clase-6/Test-Clase-6-Ejercicio-2.cy.js
@@ -0,0 +1,5 @@
+describe('Test de la Clase 6 del Ejercicio 2', () => {
+    it('Visitar la pagina ', () => {
+        cy.visit('http://192.168.100.28:8080/Clase-6/Ejercicio%202/')
+    })
+})
