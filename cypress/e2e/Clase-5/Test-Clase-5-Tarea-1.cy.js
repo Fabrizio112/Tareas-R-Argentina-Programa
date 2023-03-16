d87630cbbba4a5d543588210d6b0db0569a6e632
Fabrizio112 <fabri.avila3@gmail.com>
2023-03-16 15:36:58 -0300

    Agrega pruebas basicas de la primer tareas

--- cypress/e2e/Clase-5/Test-Clase-5-Tarea-1.cy.js
+++ cypress/e2e/Clase-5/Test-Clase-5-Tarea-1.cy.js
@@ -1,5 +1,19 @@
 describe('Test de la Clase 5 de la Tarea 1', () => {
-    it('Visitar la pagina ', () => {
+    beforeEach('Visitar la pagina ', () => {
         cy.visit('http://192.168.100.28:8080/Clase-5/')
     })
+    it('Probar si la calculadora funciona de forma correcta', () => {
+        cy.get('#salario-user').type(850000);
+        cy.get('#calcular').click();
+        expect(cy.get('#salario-mensual').should("value", (850000 / 12)));
+    })
+    it('Probar si el boton de resetear funciona de forma correcta', () => {
+        cy.get('#salario-user').type(12000);
+        cy.get('#calcular').click();
+        expect(cy.get('#salario-mensual').should("value", 1000));
+        cy.get('[type="reset"]').click()
+        expect(cy.get('#salario-user').should("value", ""))
+        expect(cy.get('#salario-mensual').should("value", ""))
+    })
+
 })
